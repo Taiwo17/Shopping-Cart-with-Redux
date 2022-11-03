2b0382da09e5f00f186e4167556725add266607f
taiwo17 <shoboadefowope2007@gmail.com>
2022-11-02 18:12:24 -0700

    Shopping cart with React-Redux Toolkit

---
+++ src/pages/Cart.js
@@ -0,0 +1,41 @@
+import './cart.css'
+import Total from '../components/Total'
+import CartItem from '../components/CartItem'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+function Cart() {
+  const cart = useSelector((state) => state.cart)
+  return (
+    <>
+      <div className='cart'>
+        <div className='cart__left'>
+          <div>
+            <h3>Shopping Cart</h3>
+            {cart?.map((item) => (
+              <CartItem
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                price={item.price}
+                quantity={item.quantity}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className='cart__right'>
+          <Total />
+        </div>
+      </div>
+      <div className='back__home'>
+        <Link to='/'>
+          <p>Back Home</p>
+        </Link>
+      </div>
+    </>
+  )
+}
+
+export default Cart
